fa896525e21e5db6f56a5ded3d4ddac01834f676
geravvene <romanov17903@gmail.com>
2024-08-18 19:38:29 +0300

    update

--- bot.py
+++ bot.py
@@ -1,29 +1,34 @@
 import asyncio
 import logging
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from aiogram import F
+from aiogram import F, Bot
 from aiogram.filters import CommandStart
 from aiogram.types import Message, CallbackQuery
 
 from aiogram.filters.callback_data import CallbackData
 from aiogram_calendar import DialogCalendar, DialogCalendarCallback
 from aiogram.fsm.context import FSMContext
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from scheduler import SchedulerMiddleware
 
 from menu import start_menu, date_menu1, date_menu2
 from func import get_count, string_range_date
 from create_bot import bot, dp, collection, Form
 
+async def send_stata(bot: Bot, chat_id):
+    await bot.send_message(text=string_range_date(datetime.now()-timedelta(days=7), datetime.now())+get_count(collection), chat_id=chat_id, parse_mode="Markdown")
 
 @dp.message(CommandStart())
-async def send_welcome(msg: Message):
+async def send_welcome(msg: Message, apscheduler: AsyncIOScheduler):
+    apscheduler.add_job(send_stata, 'cron', day_of_week='sun', hour=17, minute=52, kwargs={"bot": bot,"chat_id": msg.chat.id})
     await msg.answer(f'Привет, {msg.from_user.username}! Выберете опцию статистики:', reply_markup=start_menu)
 
 
 @dp.callback_query(F.data == "All")
 async def display_all(callback_query: CallbackQuery):
-    await callback_query.message.answer(string_range_date(False, datetime.now())+get_count(collection))
+    await callback_query.message.answer(string_range_date(False, datetime.now())+get_count(collection), parse_mode="Markdown")
 
 
 @dp.callback_query(F.data == "Range")
@@ -81,6 +86,9 @@ async def process_dialog_calendar(callback_query: CallbackQuery, callback_data: 
 
 
 async def main() -> None:
+    scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
+    scheduler.start()
+    dp.update.middleware(SchedulerMiddleware(scheduler=scheduler))
     await dp.start_polling(bot)
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
--- func.py
+++ func.py
@@ -1,39 +1,58 @@
+from tabulate import tabulate
+from aiogram.utils.markdown import bold
+
 def count(collection, date1, date2):
     pnl = 0
     symbol = {}
     length = 0
+    winrate = 0
     orders = collection.find({"DATE": {"$gte": date1, "$lte": date2}})if date1 and date2 else collection.find(
         {"DATE": {"$gte": date1}}) if date1 and not date2 else collection.find(
             {"DATE": {"$lte": date2}}) if not date1 and date2 else collection.find()
     for row in orders:
-        pnl += float(row['PROFIT']) + float(row['COMMISSION'])
+        order_pnl = float(row['PROFIT']) + float(row['COMMISSION'])
+        pnl += order_pnl
+        winrate += 1 if order_pnl > 0 else 0
         symbol[row['SYMBOL']] = symbol[row['SYMBOL']
-                                       ] if row['SYMBOL'] in symbol else {'PNL': 0, 'Кол-во': 0}
-        symbol[row['SYMBOL']]['PNL'] = symbol[row['SYMBOL']]['PNL'] + \
-            float(row['PROFIT']) + float(row['COMMISSION'])
-        symbol[row['SYMBOL']]['Кол-во'] = symbol[row['SYMBOL']]['Кол-во'] + 1
-        length += 1
+                                       ] if row['SYMBOL'] in symbol else {'all':{'PNL': 0, 'Winrate': 0, 'k/p':0}, 'Сделки': []}
+        symbol[row['SYMBOL']]['all']['PNL'] += order_pnl
+        symbol[row['SYMBOL']]['all']['Winrate'] += 1 if order_pnl > 0 else 0
+        volume = float(row['VOLUME'])
+        risk = volume*10*20
+        symbol[row['SYMBOL']]['Сделки'].append(
+            {'dt':row['DATE'].strftime('%d.%m'), 'vl': volume, '!': risk, '$': round(order_pnl,2), 'k/p': round(order_pnl/risk, 2)})
+        length+=1
 
+    risk = 0
     for key in symbol:
-        piece = symbol[key]['Кол-во']/length
-        symbol[key]['Кол-во'] = f"{symbol[key]['Кол-во']} | {round(piece*100, 2)} %"
-        symbol[key]['KPD'] = symbol[key]['PNL']/piece
-        symbol[key]['PNL'] = round(symbol[key]['PNL'], 2)
-        symbol[key]['KPD'] = round(symbol[key]['KPD'], 2)
+        risk_symbol = 0
+        for order in symbol[key]['Сделки']:
+            risk_symbol += order['!']
+        risk += risk_symbol
+        symbol[key]['all']['k/p'] = f"{round(symbol[key]['all']['PNL']/risk_symbol, 2)}"
+        symbol[key]['all']['PNL'] = f"{round(symbol[key]['all']['PNL'], 2)} $"
+        symbol[key]['all']['Winrate'] = f"{round(symbol[key]['all']['Winrate']/len(symbol[key]['Сделки'])*100, 2)} %"
 
-    return {'pnl': round(pnl, 2), 'symbol': dict(
-        sorted(symbol.items(), key=lambda item: item[1]['KPD'], reverse=True)), 'length': length}
+    return {'all': {'PNL': f"{round(pnl, 2)} $", 'Winrate': f"{round(winrate/length*100, 2)} %", 'Всего сделок': length, 'k/p': round(pnl/risk, 2)}, 'symbol': symbol}
 
 
 def get_count(collection, date1=False, date2=False):
     data = count(collection, date1, date2)
+    all=data['all']
+    str=f"{bold('Общее:', '') }\n"
+    for key in all:
+        str += f"{key} = {all[key]}\n"
+    str+="\n"
     currencies = data['symbol']
-    str = f"<b>PNL = {data['pnl']}</b>\nВсего сделок: {data['length']}\n\n"
     for currency in currencies:
-        str += f"<b>{currency}:</b>\n"
-        for key in currencies[currency]:
-            str += f"{key} = {currencies[currency][key]}\n"
+        str += f"{bold(currency+':', '')}\n"
+        for key in currencies[currency]['all']:
+            str += f"{key} = {currencies[currency]['all'][key]}\n"
         str += "\n"
+        orders= currencies[currency]['Сделки']
+        str += f"```{tabulate(orders, headers="keys", stralign='center')}```"
+        str += "\n\n"
+        
     return str
 
 
